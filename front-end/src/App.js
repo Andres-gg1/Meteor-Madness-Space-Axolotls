3cec2d0af9ac643fc999f160230a7b6a245227c2
ItzaPelona <alanhfdez2@gmail.com>
2025-10-05 20:41:28 -0600

    added new route for 2D impact damage simulation and created basic ImpactMap component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import WatchMeteorites from './pages/orbit-sim';
 import SimulateImpact from './pages/impact-sim';
+import ImpactMap from './components/ImpactMap.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/watch" element={<WatchMeteorites />} />
         <Route path="/simulate" element={<SimulateImpact />} />
+        <Route path="/impact-damage" element={<ImpactMap />} />
       </Routes>
     </Router>
   );
